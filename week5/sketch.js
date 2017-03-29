36472970e6fcf01bb86a38ff73fac0749b07b6d4
Marklegit <Marklegit@gmail.com>
2017-03-29 13:05:08 -0400

    Add files via upload
    update week5

--- week5/sketch.js
+++ week5/sketch.js
@@ -1,22 +1,42 @@
-function setup() {
-	createCanvas(640, 480);
+// windows
+function setup() { 
+   createCanvas(1000, 1000);
+    noStroke();
 }
 
 function draw() {
-	background(220);
+    background(0);
     
-    var ww = width/4;
-    var wh = height/2;
+    var windowWidth = width/6; // window width
+    var windowHeight = height/2; // window height
     
-    for (var x = ww/4; x < width; x+=ww) {
-    for (var y = wh/4; y < height; y+=wh){
-        fill(162, 82, 540);
-        rect(x,y, ww/2, wh2);
+    for ( var x = windowWidth/4; x < width; x+=windowWidth) {
+       for (var y = windowHeight/4; y < height; y+=windowHeight) {
+           
+           fill("orange");
+           rect(x, y, windowWidth/2, windowHeight/2);
+           
+           fill("yellow");
         
-        fill('black');
-        stroke(167)
-       }
+      
+           rect(x, y + windowHeight/8, windowWidth/4, windowHeight/8);
+           rect(x + windowWidth/4, y + windowHeight/8, windowWidth/4, windowHeight/8);
+         
+           fill("orange");
+           triangle(500, 320, 40, 625, 960, 626);
         
+           fill("red");
+           triangle(500, 420, 40, 625, 960, 626);
+           
+             fill("yellow");
+           triangle(500, 520, 40, 625, 960, 626);
+           
+           fill("orange");
+           rect(40, 20, 920, 100);
+           
+           fill("black");
+           rect(120, 30, 720, 70);
+        }
     }
+        
 }
-
